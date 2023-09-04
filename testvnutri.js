cc6686f6e0fe85ff0c07cd78b3cf00a5e640b1ea
loosersoft <nonexisting@everywhere.no>
2023-09-04 17:58:25 +0200

    som urbil vo vs code aby to daco robilo

--- testvnutri.js
+++ testvnutri.js
@@ -1,9 +1,11 @@
 function main() {
 
+  const myDiv = document.querySelector('#write');
+
+  myDiv.innerHTML = 'Hello world!';
+
   console.log('Existujem?');
   console.log('znova si to otestujem');
-
-  
   
 }
 
